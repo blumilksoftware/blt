3cae73c6ce430897b9aa3123120f8f956c1307e0
JakubKermes <jakubker22@gmail.com>
2024-06-06 10:48:23 +0200

    Add support for parameters and multiple jobs

--- src/Features/Traits/Dispatcher.php
+++ src/Features/Traits/Dispatcher.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Blumilk\BLT\Features\Traits;
 
+use Blumilk\BLT\Helpers\ArrayHelper;
 use Illuminate\Support\Facades\Bus;
 
 trait Dispatcher
@@ -17,25 +18,41 @@ trait Dispatcher
     }
 
     /**
-     * @Then :jobName is dispatched
-     * @Then job :jobName is dispatched
+     * @When I dispatch :count :jobName jobs with parameters: :parameters
+     * @When I dispatch :count :jobName jobs
+     * @when I dispatch :jobName job
      */
-    public function jobIsDispatched(string $jobName): void
+    public function dispatchJobs(string $jobName, int $count = 1, string|array $parameters = []): void
     {
+        $parameters = ArrayHelper::toArray($parameters);
+
         $jobClass = $this->recognizeJobClass($jobName);
-        $jobClass::dispatch();
-        Bus::assertDispatched($jobClass);
+        $job = new $jobClass(...$parameters);
+
+        for ($i = 0; $i < $count; $i++) {
+            Bus::dispatch($job);
+        }
     }
 
-    private function recognizeJobClass(string $job): string
+    /**
+     * @Then I should see :jobName job was dispatched
+     * @Then I should see :count :jobName jobs were dispatched
+     */
+    public function assertJobsDispatched(string $jobName, int $count = 1): void
     {
-        if (strpos($job, "\\")) {
-            return $job;
+        $jobClass = $this->recognizeJobClass($jobName);
+        Bus::assertDispatched($jobClass, $count);
+    }
+
+    private function recognizeJobClass(string $jobName): string
+    {
+        if (strpos($jobName, "\\")) {
+            return $jobName;
         }
 
-        $job = ucfirst($job);
+        $jobName = ucfirst($jobName);
 
-        return $this->getJobNamespace() . $job;
+        return $this->getJobNamespace() . $jobName;
     }
 
     private function getJobNamespace(): string
