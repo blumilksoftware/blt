224bd59490c397402c161e53a227c083ffda0b1f
PiotrFedak <piotr.fedak@studenci.collegiumwitelona.pl>
2024-06-10 09:50:35 +0200

    Checking UUID and Middleware Enablement

--- src/Features/Traits/Middleware.php
+++ src/Features/Traits/Middleware.php
@@ -7,8 +7,11 @@ namespace Blumilk\BLT\Features\Traits;
 use Behat\Gherkin\Node\TableNode;
 use Blumilk\BLT\LaravelContracts;
 use Closure;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Foundation\Http\Kernel;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Str;
+use PHPUnit\Framework\Assert;
 
 trait Middleware
 {
@@ -54,5 +57,48 @@ trait Middleware
     public function disableCsrfProtection(): void
     {
         $this->disableMiddleware(LaravelContracts::CSRF_PROTECTION_MIDDLEWARE_CLASS);
+    }
+
+    /**
+     * @Given there is a :middleware middleware enabled
+     */
+    public function enableMiddleware(string $middleware): void
+    {
+        $kernel = $this->getContainer()->make(Kernel::class);
+        $kernel->pushMiddleware($middleware);
+    }
+
+    /**
+     * @Then request object should have an UUID in :field field
+     */
+    public function requestShouldHaveUuidInField(string $field): void
+    {
+        $request = $this->getContainer()->make(Request::class);
+        $uuid = $request->attributes->get($field);
+
+        Assert::assertNotNull($uuid, "The request field $field does not contain a UUID.");
+        Assert::assertTrue(
+            Str::isUuid($uuid),
+            "The request field $field does not contain a valid UUID.",
+        );
+    }
+
+    /**
+     * @Given there is a middleware that adds UUID to request
+     */
+    public function addUuidMiddleware(): void
+    {
+        $this->getContainer()->singleton('App\Http\Middleware\IdentifyRequest', function () {
+            return new class() {
+                public function handle(Request $request, Closure $next): Response
+                {
+                    $request->attributes->set("id", (string)Str::uuid());
+                    return $next($request);
+                }
+            };
+        });
+
+        $kernel = $this->getContainer()->make(Kernel::class);
+        $kernel->pushMiddleware('App\Http\Middleware\IdentifyRequest');
     }
 }
