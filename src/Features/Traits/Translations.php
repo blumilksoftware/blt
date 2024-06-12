4339752510fb072b08b3ec93257b4d1a41e446c2
JakubKermes <jakubker22@gmail.com>
2024-06-12 11:20:52 +0200

    Add basic translation tools

--- src/Features/Traits/Translations.php
+++ src/Features/Traits/Translations.php
@@ -4,6 +4,37 @@ declare(strict_types=1);
 
 namespace Blumilk\BLT\Features\Traits;
 
+use Behat\Gherkin\Node\TableNode;
+use Illuminate\Contracts\Translation\Translator;
+use PHPUnit\Framework\Assert;
+
 trait Translations
 {
+    use Application;
+
+    private string $input;
+
+    /**
+     * @When I ask to translate :input
+     * @When I ask for translations of :input
+     */
+    public function askToTranslate(string $input): void
+    {
+        $this->input = $input;
+    }
+
+    /**
+     * @Then I should see the following phrases:
+     */
+    public function assertTranslationMatches(?TableNode $table = null): void
+    {
+        $translator = $this->getContainer()->get(Translator::class);
+
+        foreach ($table as $row) {
+            $this->getContainer()->setLocale($row["locale"]);
+            $translation = $translator->get($this->input);
+
+            Assert::assertEquals($row["phrase"], $translation);
+        }
+    }
 }
