952c76b77c90141a186f5b5887d9814e64f41d16
JakubKermes <jakubker22@gmail.com>
2024-06-13 09:03:04 +0200

    Add endpoint response handling

--- src/Features/Traits/View.php
+++ src/Features/Traits/View.php
@@ -5,22 +5,27 @@ declare(strict_types=1);
 namespace Blumilk\BLT\Features\Traits;
 
 use Behat\Gherkin\Node\TableNode;
+use Exception;
+use Illuminate\Contracts\Container\BindingResolutionException;
 use Illuminate\Support\Str;
 use Illuminate\View\View as LaravelView;
 use PHPUnit\Framework\Assert;
+use Symfony\Component\HttpFoundation\Request;
+use Throwable;
 
 trait View
 {
     use Application;
     use Http;
 
-    private LaravelView $view;
+    private ?LaravelView $view = null;
 
     /**
      * @Given user is looking at :page :view view
      * * @Given user is looking at :view view
      * @Given user is looking at :page :view view with:
      * @Given user is looking at :view view with:
+     * @throws BindingResolutionException
      */
     public function userIsLookingAtView(string $view, string $page = "", ?TableNode $table = null): void
     {
@@ -34,18 +39,32 @@ trait View
             }
         }
 
-        if (!$page) {
-            $this->view = $this->getContainer()->make("view")->make($view, $params);
-        } else {
-            $this->view = $this->getContainer()->make("view")->make("{$page}.{$view}");
-        }
+        $viewName = $page ? "$page.$view" : $view;
+        $this->view = $this->getContainer()->make("view")->make($viewName, $params);
     }
 
     /**
-     * @Then response should contain :data in the view
+     * @When user is accessing :endpoint endpoint
+     * @When user is accessing :endpoint endpoint using :method method
+     */
+    public function userIsAccessing(string $endpoint, string $method = Request::METHOD_GET): void
+    {
+        $this->aUserIsRequesting($endpoint, $method);
+        $this->aRequestIsSent();
+    }
+
+    /**
+     * @Then view should contain :data
+     * @throws Throwable
      */
     public function viewContains(string $data): void
     {
-        Assert::assertStringContainsString($data, $this->view->render());
+        if ($this->view) {
+            Assert::assertStringContainsString($data, $this->view->render());
+        } elseif ($this->response) {
+            Assert::assertStringContainsString($data, $this->response->getContent());
+        } else {
+            throw new Exception("No view or response found");
+        }
     }
 }
