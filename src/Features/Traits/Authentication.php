8c369db4c99a4c2caed3b0b4ba1c6f4b1a3b0718
PiotrFedak <piotr.fedak@studenci.collegiumwitelona.pl>
2024-06-17 14:41:38 +0200

    authentication feature extended

--- src/Features/Traits/Authentication.php
+++ src/Features/Traits/Authentication.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Blumilk\BLT\Features\Traits;
 
+use Behat\Gherkin\Node\TableNode;
 use Illuminate\Contracts\Auth\Guard;
 use Illuminate\Contracts\Container\BindingResolutionException;
 use PHPUnit\Framework\Assert;
@@ -50,6 +51,67 @@ trait Authentication
     {
         $auth = $this->getContainer()->make(Guard::class);
         Assert::assertEquals($value, $auth->user()?->{$field});
+    }
+
+    /**
+     * @Given there is an authenticated user with email :email
+     * @throws BindingResolutionException
+     */
+    public function thereIsAuthenticatedUserWithEmail(string $email): void
+    {
+        $auth = $this->getContainer()->make(Guard::class);
+        $auth->login($this->getUserModel()::query()->where("email", $email)->first());
+    }
+
+    /**
+     * @Given there are users in the database:
+     */
+    public function thereAreUsersInTheDatabase(TableNode $table): void
+    {
+        foreach ($table->getColumnsHash() as $userData) {
+            $this->getUserModel()::firstOrCreate($userData);
+        }
+    }
+
+    /**
+     * @Given there is an unauthenticated user
+     * @throws BindingResolutionException
+     */
+    public function thereIsAnUnauthenticatedUser(): void
+    {
+        $this->thereIsNoUserAuthenticatedInSession();
+    }
+
+    /**
+     * @Then the authenticated user's email should be :email
+     * @throws BindingResolutionException
+     */
+    public function authenticatedUsersEmailShouldBe(string $email): void
+    {
+        $auth = $this->getContainer()->make(Guard::class);
+        Assert::assertEquals($email, $auth->user()->email);
+    }
+
+    /**
+     * @Then the authenticated user should have the attribute :attribute with value :value
+     * @throws BindingResolutionException
+     */
+    public function authenticatedUserShouldHaveAttribute(string $attribute, $value): void
+    {
+        $auth = $this->getContainer()->make(Guard::class);
+        $user = $auth->user();
+        Assert::assertEquals($value, $user->{$attribute});
+    }
+
+    /**
+     * @Given the user is authenticated with attributes:
+     */
+    public function userIsAuthenticatedWithAttributes(TableNode $table): void
+    {
+        $attributes = $table->getRowsHash();
+        $user = $this->getUserModel()::firstOrCreate($attributes);
+        $auth = $this->getContainer()->make(Guard::class);
+        $auth->login($user);
     }
 
     /**
