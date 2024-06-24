367142f3d43acd8bc29dd11c6da04e1a3f6ef64e
JakubKermes <jakubker22@gmail.com>
2024-06-24 15:28:38 +0200

    csf

--- src/Features/SpatiePermission.php
+++ src/Features/SpatiePermission.php
@@ -1,10 +1,11 @@
 <?php
+
 declare(strict_types=1);
+
 namespace Blumilk\BLT\Features;
 
 use Behat\Behat\Context\Context;
 use Blumilk\BLT\Features\Traits\Optional\SpatiePermission as SpatiePermissionTrait;
-
 
 class SpatiePermission implements Context
 {
