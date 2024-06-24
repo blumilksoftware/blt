46a115c300f74ac0f3b913a31732be8950ec808d
Jakub Kermes <114694305+JakubKermes@users.noreply.github.com>
2024-06-24 13:33:04 +0200

    - Add dispatcher tools (#32)
    * Add basic dispatcher tools
    
    * added forgotten ,
    
    * Add support for parameters and multiple jobs
    
    * Add helper for recognising classes
    
    * unify Dispatcher.php working
    
    * Fix function naming
    
    * Go from facades to contracts
    
    * Add support for custom namespaces to RecognizeClassHelper.php
    
    * Update Dispatcher.php
    
    * optimize guessType function
    
    * Change helper to work with models
    
    * suggested changes
    
    * suggested changes
    
    ---------
    
    Co-authored-by: PiotrFedak <piotr.fedak@studenci.collegiumwitelona.pl>

--- src/Features/Traits/Dispatcher.php
+++ src/Features/Traits/Dispatcher.php
@@ -4,6 +4,95 @@ declare(strict_types=1);
 
 namespace Blumilk\BLT\Features\Traits;
 
+use Behat\Gherkin\Node\TableNode;
+use Blumilk\BLT\Helpers\RecognizeClassHelper;
+use Blumilk\BLT\Helpers\TypesEnum;
+use Illuminate\Contracts\Bus\Dispatcher as BusDispatcher;
+use Illuminate\Contracts\Container\BindingResolutionException;
+use Illuminate\Contracts\Events\Dispatcher as EventDispatcher;
+use Illuminate\Support\Testing\Fakes\BusFake;
+use Illuminate\Support\Testing\Fakes\EventFake;
+use InvalidArgumentException;
+
 trait Dispatcher
 {
+    use Application;
+
+    protected BusFake $busFake;
+    protected EventFake $eventFake;
+
+    /**
+     * @Given bus is running
+     * @throws BindingResolutionException
+     */
+    public function runBus(): void
+    {
+        $this->busFake = new BusFake($this->getContainer()->make(BusDispatcher::class));
+    }
+
+    /**
+     * @Given events are faked
+     * @throws BindingResolutionException
+     */
+    public function fakeEvents(): void
+    {
+        $this->eventFake = new EventFake($this->getContainer()->make(EventDispatcher::class));
+    }
+
+    /**
+     * @When I dispatch :count :objectName jobs with parameters:
+     * @When I dispatch :count :objectName jobs
+     * @when I dispatch :objectName job
+     * @When I dispatch :count of :objectName events with parameters:
+     * @When I dispatch :count of :objectName events
+     * @when I dispatch :objectName event
+     * @When I dispatch :count of :objectName with parameters:
+     * @When I dispatch :count of :objectName
+     * @when I dispatch :objectName
+     */
+    public function dispatchObject(string $objectName, ?TableNode $table = null, int $count = 1): void
+    {
+        $parameters = [];
+
+        if ($table) {
+            foreach ($table as $row) {
+                $value = is_numeric($row["value"]) ? (int)$row["value"] : (string)$row["value"];
+                $parameters[] = $value;
+            }
+        }
+
+        $objectClass = RecognizeClassHelper::recognizeObjectClass($objectName);
+
+        $object = new $objectClass(...$parameters);
+
+        for ($i = 0; $i < $count; $i++) {
+            $this->resolveFaker($objectName)->dispatch($object);
+        }
+    }
+
+    /**
+     * @Then I should see :objectName event was dispatched
+     * @Then I should see :count of :objectName events were dispatched
+     * @Then I should see :objectName job was dispatched
+     * @Then I should see :count of :objectName jobs were dispatched
+     * @Then I should see :objectName was dispatched
+     * @Then I should see :count of :objectName were dispatched
+     */
+    public function assertDispatched(string $objectName, int $count = 1): void
+    {
+        $objectClass = RecognizeClassHelper::recognizeObjectClass($objectName);
+
+        $this->resolveFaker($objectName)->assertDispatched($objectClass, $count);
+    }
+
+    protected function resolveFaker(string $objectName): BusFake|EventFake
+    {
+        $objectType = RecognizeClassHelper::guessType($objectName);
+
+        return match ($objectType) {
+            TypesEnum::Job->value => $this->busFake,
+            TypesEnum::Event->value => $this->eventFake,
+            default => throw new InvalidArgumentException("Unsupported object type: $objectType"),
+        };
+    }
 }
---
+++ src/Helpers/RecognizeClassHelper.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Blumilk\BLT\Helpers;
+
+use Illuminate\Support\Str;
+
+class RecognizeClassHelper
+{
+    public static function recognizeObjectClass(string $objectName): string
+    {
+        if (strpos($objectName, "\\")) {
+            return $objectName;
+        }
+
+        $objectName = Str::ucfirst(Str::singular($objectName));
+
+        return self::getObjectNamespace($objectName) . $objectName;
+    }
+
+    public static function getObjectNamespace(string $objectName): string
+    {
+        $type = self::guessType($objectName);
+        $typeNamespace = self::getNamespaceFromConfig($objectName, $type);
+
+        if ($typeNamespace) {
+            return $typeNamespace;
+        }
+
+        $type = Str::plural(Str::ucfirst($type));
+        $defaultNamespace = config("blt.namespaces.default", "App\\");
+
+        return $defaultNamespace . $type . "\\";
+    }
+
+    public static function guessType(string $objectName): string
+    {
+        $slug = Str::slug($objectName);
+
+        foreach (TypesEnum::cases() as $objectType) {
+            if (Str::contains($slug, $objectType->value)) {
+                return Str::singular($objectType->value);
+            }
+        }
+
+        return "model";
+    }
+
+    protected static function getNamespaceFromConfig(string $objectName, string $type): ?string
+    {
+        $typeNamespaces = config("blt.namespaces.types");
+
+        return $typeNamespaces[$objectName] ?? $typeNamespaces[$type] ?? null;
+    }
+}
