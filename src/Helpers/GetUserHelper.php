3b899755e0c2a09c6af09b0d87ba7c589f6f6749
JakubKermes <jakubker22@gmail.com>
2024-06-24 22:06:15 +0200

    Add helper

---
+++ src/Helpers/GetUserHelper.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Blumilk\BLT\Helpers;
+
+class GetUserHelper
+{
+    public static function getUser( string $field,string $value): object
+    {
+        $userClass = RecognizeClassHelper::recognizeObjectClass("User");
+
+        return $userClass::query()->where($field, $value)->first();
+    }
+}
