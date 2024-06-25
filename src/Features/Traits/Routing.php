6086350602586e78301e8141f7f89a7d591c89eb
PiotrFedak <piotr.fedak@studenci.collegiumwitelona.pl>
2024-06-25 16:34:35 +0200

    Combine HttpRequest, HttpResponse, Session, Authentication into Routing trait

---
+++ src/Features/Traits/Routing.php
@@ -0,0 +1,189 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Blumilk\BLT\Features\Traits;
+
+use Behat\Gherkin\Node\TableNode;
+use Illuminate\Contracts\Auth\Guard;
+use Illuminate\Contracts\Container\BindingResolutionException;
+use PHPUnit\Framework\Assert;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+trait Routing
+{
+    use HttpRequest, HttpResponse, Session, Authentication; //Also translation, waiting for PR to merge.
+
+    /**
+     * @Given a user is authenticated with email :email
+     * @throws BindingResolutionException
+     */
+    public function aUserIsAuthenticatedWithEmail(string $email): void
+    {
+        $this->thereIsAuthenticatedUserWithEmail($email);
+    }
+
+    /**
+     * @Given a user is requesting :url
+     * @Given a user is requesting :url using :method method
+     */
+    public function aUserIsRequesting(string $url, string $method = Request::METHOD_GET): void
+    {
+        $this->request = Request::create($url, $method);
+    }
+
+    /**
+     * @When a request is sent
+     */
+    public function aRequestIsSent(): void
+    {
+        $this->response = $this->getContainer()->handle($this->request);
+    }
+
+    /**
+     * @Then a response status code should be :status
+     */
+    public function aResponseStatusCodeShouldBe(int $status): void
+    {
+        Assert::assertEquals($status, $this->response->getStatusCode());
+    }
+
+    /**
+     * @Then the response should contain JSON:
+     */
+    public function aResponseShouldContainJson(TableNode $table): void
+    {
+        $json = json_decode($this->response->getContent(), true);
+
+        foreach ($table->getRowsHash() as $key => $value) {
+            $actualValue = data_get($json, $key);
+            Assert::assertNotNull($actualValue, "Key '$key' not found in response JSON.");
+            Assert::assertEquals($value, $actualValue);
+        }
+    }
+
+    /**
+     * @Given there are users in the database:
+     */
+    public function thereAreUsersInTheDatabase(TableNode $table): void
+    {
+        foreach ($table->getColumnsHash() as $userData) {
+            $this->getUserModel()::firstOrCreate($userData);
+        }
+    }
+
+    /**
+     * @Given there is an authenticated user with email :email
+     * @throws BindingResolutionException
+     */
+    public function thereIsAuthenticatedUserWithEmail(string $email): void
+    {
+        $auth = $this->getContainer()->make(Guard::class);
+        $auth->login($this->getUserModel()::query()->where('email', $email)->first());
+    }
+
+    /**
+     * @Given there is an unauthenticated user
+     * @throws BindingResolutionException
+     */
+    public function thereIsAnUnauthenticatedUser(): void
+    {
+        $this->thereIsNoUserAuthenticatedInSession();
+    }
+
+    /**
+     * @Given user is authenticated in session as :value in :field field
+     * @throws BindingResolutionException
+     */
+    public function userIsAuthenticatedInSessionAs(string $value, string $field): void
+    {
+        $auth = $this->getContainer()->make(Guard::class);
+        $auth->login($this->getUserModel()::query()->where($field, $value)->first());
+    }
+
+    /**
+     * @Given there is no user authenticated in session
+     * @throws BindingResolutionException
+     */
+    public function thereIsNoUserAuthenticatedInSession(): void
+    {
+        $auth = $this->getContainer()->make(Guard::class);
+        $auth?->logout();
+    }
+
+    /**
+     * @Then no user is authenticated in session
+     * @throws BindingResolutionException
+     */
+    public function noUserAuthenticatedInSession(): void
+    {
+        $auth = $this->getContainer()->make(Guard::class);
+        Assert::assertNull($auth->user());
+    }
+
+    /**
+     * @Then user authenticated in session has :value value in :field field
+     * @throws BindingResolutionException
+     */
+    public function userAuthenticatedInSessionIs(string $value, string $field): void
+    {
+        $auth = $this->getContainer()->make(Guard::class);
+        Assert::assertEquals($value, $auth->user()?->{$field});
+    }
+
+    /**
+     * @Then the authenticated user's email should be :email
+     * @throws BindingResolutionException
+     */
+    public function authenticatedUsersEmailShouldBe(string $email): void
+    {
+        $auth = $this->getContainer()->make(Guard::class);
+        Assert::assertEquals($email, $auth->user()->email);
+    }
+
+    /**
+     * @Then the authenticated user should have the attribute :attribute with value :value
+     * @throws BindingResolutionException
+     */
+    public function authenticatedUserShouldHaveAttribute(string $attribute, $value): void
+    {
+        $auth = $this->getContainer()->make(Guard::class);
+        $user = $auth->user();
+        Assert::assertEquals($value, $user->{$attribute});
+    }
+
+    /**
+     * @Given the user is authenticated with attributes:
+     */
+    public function userIsAuthenticatedWithAttributes(TableNode $table): void
+    {
+        $attributes = $table->getRowsHash();
+        $user = $this->getUserModel()::firstOrCreate($attributes);
+        $auth = $this->getContainer()->make(Guard::class);
+        $auth->login($user);
+    }
+
+    /**
+     * @Then the user with email :email should not be able to access :url
+     * @throws BindingResolutionException
+     */
+    public function userWithEmailShouldNotBeAbleToAccess(string $email, string $url): void
+    {
+        $auth = $this->getContainer()->make(Guard::class);
+        $auth->login($this->getUserModel()::query()->where("email", $email)->first());
+        $response = $this->call("GET", $url);
+        Assert::assertEquals(Response::HTTP_FORBIDDEN, $response->status());
+    }
+
+    /**
+     * @Then the user should be able to logout
+     * @throws BindingResolutionException
+     */
+    public function userShouldBeAbleToLogout(): void
+    {
+        $auth = $this->getContainer()->make(Guard::class);
+        $auth->logout();
+        Assert::assertNull($auth->user());
+    }
+}
