ddda6ec150358b462534449fbe9fbe2b5ca03b2f
JakubKermes <jakubker22@gmail.com>
2024-06-27 12:35:49 +0200

    add missing helper

--- config/blt.php
+++ config/blt.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 use Blumilk\BLT\Helpers\ArrayHelper;
+use Blumilk\BLT\Helpers\BooleanHelper;
 use Blumilk\BLT\Helpers\ClassHelper;
 use Blumilk\BLT\Helpers\ContextHelper;
 use Blumilk\BLT\Helpers\NullableHelper;
@@ -13,6 +14,7 @@ return [
     ],
     "helpers" => [
         "array" => ArrayHelper::class,
+        "boolean" => BooleanHelper::class,
         "class" => ClassHelper::class,
         "context" => ContextHelper::class,
         "nullable" => NullableHelper::class,
