4005313c3c0343121373c1635d90c284e54b370b
Jakub Kermes <114694305+JakubKermes@users.noreply.github.com>
2024-06-28 13:49:01 +0200

    #56 - add notification tools (#62)

--- src/Features/Toolbox.php
+++ src/Features/Toolbox.php
@@ -13,6 +13,7 @@ use Blumilk\BLT\Features\Traits\Eloquent;
 use Blumilk\BLT\Features\Traits\Environment;
 use Blumilk\BLT\Features\Traits\Http;
 use Blumilk\BLT\Features\Traits\Middleware;
+use Blumilk\BLT\Features\Traits\Notification;
 use Blumilk\BLT\Features\Traits\Session;
 use Blumilk\BLT\Features\Traits\Testing;
 use Blumilk\BLT\Features\Traits\Translations;
@@ -30,4 +31,5 @@ class Toolbox implements Context
     use Session;
     use Testing;
     use Translations;
+    use Notification;
 }
