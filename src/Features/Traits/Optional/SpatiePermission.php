eb328820e29cbeca0536d9bcf073b339c77d69f3
JakubKermes <jakubker22@gmail.com>
2024-06-28 11:06:09 +0200

    extend laravel-permission tools

--- src/Features/Traits/Optional/SpatiePermission.php
+++ src/Features/Traits/Optional/SpatiePermission.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Blumilk\BLT\Features\Traits\Optional;
 
+use Behat\Gherkin\Node\TableNode;
 use Blumilk\BLT\Features\Traits\Application;
 use Blumilk\BLT\Helpers\RecognizeClassHelper;
 use Illuminate\Contracts\Container\BindingResolutionException;
@@ -26,7 +27,7 @@ trait SpatiePermission
     }
 
     /**
-     * @Then assign :role role to :object with :value value in :field field
+     * @Given :object with :value value in :field field has :role role
      */
     public function assignRoleTo(string $role, string $object, string $value, string $field): void
     {
@@ -35,12 +36,63 @@ trait SpatiePermission
     }
 
     /**
-     * @Then give :permission permission to :object with :value value in :field field
+     * @Given :object with :value value in :field field :permission permission has been revoked
+     */
+    public function revokePermissionTo(string $permission, string $value, string $field, string $object): void
+    {
+        $object = $this->getObjectInstance($object, $value, $field);
+        $object->revokePermissionTo($permission);
+    }
+
+    /**
+     * @Given :object with :value value in :field field :role role has been removed
+     */
+    public function revokeRoleTo(string $role, string $object, string $value, string $field): void
+    {
+        $object = $this->getObjectInstance($object, $value, $field);
+        $object->removeRole($role);
+    }
+
+    /**
+     * @Given :object with :value value in :field field has :permission permission
      */
     public function givePermissionTo(string $permission, string $value, string $field, string $object): void
     {
         $object = $this->getObjectInstance($object, $value, $field);
         $object->givePermissionTo($permission);
+    }
+
+    /**
+     * @Then :object with :value value in :field field should have :role role
+     */
+    public function assertHasRole(string $object, string $field, string $value, string $role): void
+    {
+        $object = $this->getObjectInstance($object, $value, $field);
+        Assert::assertTrue($object->hasRole($role));
+    }
+
+    /**
+     * @Then :object with :value value in :field field should have :permission permission
+     */
+    public function assertHasPermission(string $object, string $field, string $value, string $permission): void
+    {
+        $object = $this->getObjectInstance($object, $value, $field);
+        Assert::assertTrue($object->hasPermissionTo($permission));
+    }
+
+    /**
+     * @Then :object with :value value in :field field should have:
+     */
+    public function assertHas(string $object, string $field, string $value, TableNode $table): void
+    {
+        $object = $this->getObjectInstance($object, $value, $field);
+
+        foreach ($table as $row) {
+            match ($row["key"]) {
+                "role" => Assert::assertTrue($object->hasRole($row["value"])),
+                "permission" => Assert::assertTrue($object->hasPermissionTo($row["value"])),
+            };
+        }
     }
 
     private function getObjectInstance(string $object, string $value, string $field): object
