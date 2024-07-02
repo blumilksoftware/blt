bde60541bbfd45c06e77dba0116b73f3f21cbc58
PiotrFedak <piotr.fedak@studenci.collegiumwitelona.pl>
2024-07-02 10:54:29 +0200

    Added toggle between php and Gherkin, also small fixes
    Co-Authored-By: Jakub Kermes <114694305+JakubKermes@users.noreply.github.com>

--- docs/index.js
+++ docs/index.js
@@ -67,14 +67,88 @@ class SectionHeader extends HTMLElement {
 
 class CodeSnippet extends HTMLElement {
     connectedCallback() {
-        const content = this.innerHTML;
+        const gherkinText = this.getAttribute('gherkin');
+        const phpText = this.getAttribute('php');
+
         this.innerHTML = `
             <div class="my-2 bg-black text-white text-sm px-4 py-3 rounded shadow-lg">
-                <pre>${content}</pre>
+                <div class="mb-4 border-b border-gray-200 dark:border-gray-700">
+                    <ul class="flex flex-wrap -mb-px text-sm font-medium text-center" id="default-tab"
+                        data-tabs-toggle="#default-tab-content" role="tablist">
+                        <li class="me-2" role="presentation">
+                            <button class="inline-block p-4 border-b-2 rounded-t-lg" id="gherkin-tab"
+                                    data-tabs-target="#gherkin" type="button" role="tab" aria-controls="gherkin"
+                                    aria-selected="true">Gherkin
+                            </button>
+                        </li>
+                        <li class="me-2" role="presentation">
+                            <button
+                                class="inline-block p-4 border-b-2 rounded-t-lg border-transparent hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
+                                id="php-tab" data-tabs-target="#php" type="button" role="tab" aria-controls="php"
+                                aria-selected="false">PHP
+                            </button>
+                        </li>
+                    </ul>
+                </div>
+                <div id="default-tab-content">
+                    <div class="p-4" id="gherkin" role="tabpanel"
+                         aria-labelledby="gherkin-tab">
+                        <pre>${gherkinText}</pre>
+                    </div>
+                    <div class="hidden p-4" id="php" role="tabpanel"
+                         aria-labelledby="php-tab">
+                        <pre>${phpText}</pre>
+                    </div>
+                </div>
             </div>
-        `;
+       `;
+
+        this.querySelectorAll('button[data-tabs-target]').forEach(button => {
+            button.addEventListener('click', this.highlightActive.bind(this));
+        });
+
+        this.setDefaultActive();
+    }
+
+    setDefaultActive() {
+        const gherkinTab = this.querySelector('#gherkin-tab');
+        const gherkinPanel = this.querySelector('#gherkin');
+
+        if (gherkinTab && gherkinPanel) {
+            gherkinTab.classList.add('border-blue-500', 'text-blue-600');
+            gherkinTab.classList.remove('border-transparent');
+            gherkinPanel.classList.remove('hidden');
+        } else {
+            console.error('Default Gherkin tab or panel not found.');
+        }
+    }
+
+    highlightActive(event) {
+        const tabButtons = this.querySelectorAll('button[data-tabs-target]');
+        const tabPanels = this.querySelectorAll('#default-tab-content > div');
+
+        tabButtons.forEach(button => {
+            button.classList.remove('border-blue-500', 'text-blue-600');
+            button.classList.add('border-transparent');
+        });
+
+        tabPanels.forEach(panel => {
+            panel.classList.add('hidden');
+        });
+
+        const target = event.currentTarget.getAttribute('data-tabs-target');
+        const targetPanel = this.querySelector(target);
+
+        if (targetPanel) {
+            event.currentTarget.classList.add('border-blue-500', 'text-blue-600');
+            event.currentTarget.classList.remove('border-transparent');
+            targetPanel.classList.remove('hidden');
+        } else {
+            console.error(`Element with selector "${target}" not found.`);
+        }
     }
 }
+
 
 customElements.define('sidebar-header', SidebarHeader);
 customElements.define('sidebar-element', SidebarElement);
