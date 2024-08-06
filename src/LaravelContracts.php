c33c0115c57899e9171ab647949659e3adecbca5
PiotrFedak <piotr.fedak@studenci.collegiumwitelona.pl>
2024-08-06 23:07:27 +0200

    Naming fix

--- src/LaravelContracts.php
+++ src/LaravelContracts.php
@@ -8,7 +8,7 @@ use Illuminate\Routing\Middleware\ThrottleRequests;
 
 class LaravelContracts
 {
-    public const THROTTLING_MIDDLEWARE_CLASS = ThrottleRequests::class;
+    public const THROTTLING_MIDDLEWARE_CLASS = "Illuminate\Routing\Middleware\ThrottleRequests";
     public const CSRF_PROTECTION_MIDDLEWARE_CLASS = "Illuminate\Foundation\Http\Middleware\VerifyCsrfToken";
     public const LOAD_CONFIGURATION_CLASS = "Illuminate\Foundation\Bootstrap\LoadConfiguration";
 }
