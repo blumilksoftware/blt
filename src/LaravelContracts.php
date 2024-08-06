e2f2abc998c1c8062c18035ad0c59116f2246c1e
PiotrFedak <piotr.fedak@studenci.collegiumwitelona.pl>
2024-08-06 23:08:41 +0200

    small fixes

--- src/LaravelContracts.php
+++ src/LaravelContracts.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace Blumilk\BLT;
 
-use Illuminate\Routing\Middleware\ThrottleRequests;
-
 class LaravelContracts
 {
     public const THROTTLING_MIDDLEWARE_CLASS = "Illuminate\Routing\Middleware\ThrottleRequests";
